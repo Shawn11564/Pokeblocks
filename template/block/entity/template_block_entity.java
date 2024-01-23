0c532a10bac4163f18fc0b5017edd43b80070d8e
Shawn11564 <shawnmicheldev@gmail.com>
2024-01-23 15:24:57 -0500

    Update template

---
+++ template/block/entity/template_block_entity.java
@@ -0,0 +1,12 @@
+package dev.mrshawn.pokeblocks.block.entity.sentret;
+
+import dev.mrshawn.pokeblocks.block.entity.BlockEntityTypeRegistry;
+import dev.mrshawn.pokeblocks.block.entity.PokedollBlockEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+
+public class Pokedoll<Pokemon name>BlockEntity extends PokedollBlockEntity {
+	public Pokedoll<Pokemon name>BlockEntity(BlockPos pos, BlockState state) {
+		super(BlockEntityTypeRegistry.get(Pokedoll<Pokemon name>BlockEntity.class), pos, state);
+	}
+}
