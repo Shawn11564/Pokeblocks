d32aa5f4e94e3833e44c5393be145292171ed139
Shawn11564 <shawnmicheldev@gmail.com>
2026-02-17 14:20:02 -0500

    Get forge module to work properly

--- forge/src/main/java/dev/mrshawn/pokeblocks/PokeblocksForge.java
+++ forge/src/main/java/dev/mrshawn/pokeblocks/PokeblocksForge.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,20 +17,20 @@ import net.minecraftforge.registries.DeferredRegister;
 
 @Mod(PokeblocksCommon.MOD_ID)
 public final class PokeblocksForge {
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registries.BLOCK, PokeblocksCommon.MOD_ID);
-    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, PokeblocksCommon.MOD_ID);
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PokeblocksCommon.MOD_ID);
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, PokeblocksCommon.MOD_ID);
+	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registries.BLOCK, PokeblocksCommon.MOD_ID);
+	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, PokeblocksCommon.MOD_ID);
+	public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PokeblocksCommon.MOD_ID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, PokeblocksCommon.MOD_ID);
 
-    public PokeblocksForge() {
-        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	public PokeblocksForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        BLOCKS.register(modEventBus);
-        BLOCK_ENTITIES.register(modEventBus);
-        CREATIVE_TABS.register(modEventBus);
-        ITEMS.register(modEventBus);
+		BLOCKS.register(modEventBus);
+		BLOCK_ENTITIES.register(modEventBus);
+		CREATIVE_TABS.register(modEventBus);
+		ITEMS.register(modEventBus);
 
-        PokeblocksCommon.doRegistrations();
+		PokeblocksCommon.doRegistrations();
 
 		MinecraftForge.EVENT_BUS.register(this);
 	}
@@ -37,5 +38,10 @@ public final class PokeblocksForge {
 	@SubscribeEvent
 	public void onRegisterCommands(RegisterCommandsEvent event) {
 		ModCommands.registerCommands(event.getDispatcher());
+	}
+
+	@SubscribeEvent
+	public void onServerStarted(ServerStartedEvent event) {
+		PokeblocksServerLifecycle.onServerStarted(event.getServer());
 	}
 }
